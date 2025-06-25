b6d39260a6e49a8139f7522840c02e886cc04818
carolineeeesouzaaaa <125361828+carolineeeesouzaaaa@users.noreply.github.com>
2025-06-25 19:00:51 -0300

    aula 6

---
+++ script.js
@@ -0,0 +1,7 @@
+function inserirNome () {
+    let nomeUsuario = prompt("Qual o seu nome");
+    let elememento = document.querySelector("#nome-usuario");
+    elemento.textContent = nomeUsuario;
+}
+
+inserirNome();
