b181cbd6bc34a2efac6902323431c2a860fbfbae
carolineeeesouzaaaa <125361828+carolineeeesouzaaaa@users.noreply.github.com>
2025-06-25 19:13:15 -0300

    script

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function inserirNome () {
     let nomeUsuario = prompt("Qual o seu nome");
-    let elememento = document.querySelector("#nome-usuario");
+    let elemento = document.querySelector("#nome-usuario");
     elemento.textContent = nomeUsuario;
 }
 
